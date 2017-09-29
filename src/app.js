222dffe2e28d31fb6e4de4c7d099b8f070d69ba2
glitcha <colin.piggott@gmail.com>
2017-09-29 23:46:51 +0100

    You can roll initiative

--- src/app.js
+++ src/app.js
@@ -30,11 +30,15 @@ var app = {
     },
     
     init : function() {
-        app.setupAddNPCPreset($('#nps select[name="nps-preset"]'));
-
+        app.setupAddPreset($('#characters select[name="nps-preset"]'));
+        app.setupRollInitiative();
     },
     
-    setupAddNPCPreset : function(element) {
+    setupRollInitiative : function() {
+        $('#btn-roll-initiative').on('click', app.rollInitiative_OnClick);
+    },
+    
+    setupAddPreset : function(element) {
         $(element).html('');
         $(element).append($('<option>', {
             value: '',
@@ -46,19 +50,73 @@ var app = {
                 text: preset.title
             }));
         });
-        $(element).on('click', app.addNPCPreset_OnClick);
+        $(element).on('click', app.addPreset_OnClick);
     },
     
-    addNPCPreset_OnClick : function(event) {
+    addPreset_OnClick : function(event) {
         var selected = $(event.target).val();
         var preset = app.presetData[selected];
-        $('#nps .list').append(app.template('tmpl-character', preset));
+        preset.guid = app.guid();
+        $('#characters .list').append(app.template('tmpl-character', preset));
     },
 
     template : function(elementId, variables) {
         var source   = $('#' + elementId).html();
         var template = Handlebars.compile(source);
         return template(variables);
+    },
+    
+    rollInitiative_OnClick : function() {
+        
+        var rolls = [];
+        var character = $('#characters .character');
+        var at_to = 0;
+        var roll = 0;
+        var guid = '';
+        
+        for(var i = 0; i < character.length; i++) {
+            at_to = parseInt($(character[i]).find('input[name="attribute-toughness"]').val());
+            guid = $(character[i]).find('input[name="guid"]').val();
+            roll = app.rollDice(10);
+            rolls.push({
+                guid : guid,
+                roll : roll + at_to,
+                at_to : at_to
+            });
+        }
+        
+        rolls.sort(function(a, b) {
+            // if(b.roll = a.roll) 
+            //     return 
+            return parseFloat(b.roll) - parseFloat(a.roll);
+        });
+        
+        for(i = 0; i < rolls.length; i++) { 
+            if(i > 0) {
+                app.getCharacterFromGUID(rolls[i].guid).insertAfter(
+                    app.getCharacterFromGUID(rolls[i-1].guid)
+                );
+            }
+        }
+        
+        console.log(rolls);
+    },
+    
+    getCharacterFromGUID : function(guid) {
+        return $('input[value="' + guid + '"').parents('.character');  
+    },
+    
+    rollDice : function(sides) {
+        with(Math) return 1 + floor(random() * sides);
+    },
+    
+    guid : function() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
     }
 }
 
